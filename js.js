c5f20cb12800c93dd5283ae90d87a7a98045f996
LexSokolov <230267@bk.ru>
2019-08-24 12:14:59 +0300

    начало

---
+++ js.js
@@ -0,0 +1,12 @@
+
+var frederick = {
+    name: "Frederick Douglass",
+    canRead: false,
+    canWrite: false
+};
+
+function selfEducate() {
+    frederick.canRead = true
+    frederick.canWrite = true
+    return frederick
+}
